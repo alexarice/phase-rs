defed6ba604e6bd6bab9f9b7a5d195fb2aac927a
Alex Rice <alexrice999@hotmail.co.uk>
2025-08-06 10:44:45 +0100

    inline circuit evaluation

--- src/combinator/typed_syntax/mod.rs
+++ src/combinator/typed_syntax/mod.rs
@@ -6,7 +6,7 @@
 use std::iter::Sum;
 
 use super::normal_syntax::{AtomN, Buildable, PatternN};
-use crate::{ket::KetState, phase::Phase};
+use crate::{combinator::circuit_syntax::{ClauseC, PatternC, TermC}, ket::KetState, phase::Phase};
 
 /// A unitary type "qn <-> qn"
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -138,6 +138,36 @@ impl PatternT {
             PatternT::Unitary(inner) => inner.eval(),
         }
     }
+
+    fn eval_circ(&self, pattern: &mut PatternC, inj: &mut Vec<usize>, clauses: &mut Vec<ClauseC>) {
+        match self {
+            PatternT::Comp(patterns) => {
+                for p in patterns {
+                    p.eval_circ(pattern, inj, clauses);
+                }
+            }
+            PatternT::Tensor(patterns) => {
+                let mut stack: Vec<Vec<usize>> = Vec::new();
+                for p in patterns.iter().rev() {
+                    let size = p.get_type().0;
+                    let mut i = inj.split_off(inj.len() - size);
+                    p.eval_circ(pattern, &mut i, clauses);
+                    stack.push(i);
+                }
+                while let Some(i) = stack.pop() {
+                    inj.extend(i);
+                }
+            }
+            PatternT::Ket(states) => {
+                for (state, i) in states.iter().zip(inj.drain(0..states.len())) {
+                    pattern.parts[i] = Some(*state)
+                }
+            }
+            PatternT::Unitary(inner) => {
+                inner.eval_circ_clause(pattern, inj, -1.0, clauses);
+            }
+        }
+    }
 }
 
 impl TermT {
@@ -179,4 +209,80 @@ impl TermT {
             TermT::Sqrt(inner) => inner.eval_with_phase_mul(phase_mul / 2.0),
         }
     }
+
+    /// Returns a `TermC` representing the "circuit-normal-form" of the term.
+    pub fn eval_circ(&self) -> TermC {
+        let mut clauses = vec![];
+        let size = self.get_type().0;
+        let inj = (0..size).collect::<Vec<_>>();
+        self.eval_circ_clause(&PatternC::id(size), &inj, 1.0, &mut clauses);
+        TermC {
+            clauses,
+            ty: self.get_type(),
+        }
+    }
+    fn eval_circ_clause(
+        &self,
+        pattern: &PatternC,
+        inj: &[usize],
+        phase_mul: f64,
+        clauses: &mut Vec<ClauseC>,
+    ) {
+        match self {
+            TermT::Comp(terms) => {
+                if phase_mul < 0.0 {
+                    for t in terms.iter().rev() {
+                        t.eval_circ_clause(pattern, inj, phase_mul, clauses);
+                    }
+                } else {
+                    for t in terms {
+                        t.eval_circ_clause(pattern, inj, phase_mul, clauses);
+                    }
+                }
+            }
+            TermT::Tensor(terms) => {
+                let mut start = 0;
+                for t in terms {
+                    let size = t.get_type().0;
+                    let end = start + size;
+                    t.eval_circ_clause(pattern, &inj[start..end], phase_mul, clauses);
+                    start = end;
+                }
+            }
+            TermT::Id(_) => {
+                // Intentionally blank
+            }
+            TermT::Phase(phase) => {
+                clauses.push(ClauseC {
+                    pattern: pattern.clone(),
+                    phase: phase_mul * phase.eval(),
+                });
+            }
+            TermT::IfLet {
+                pattern: if_pattern,
+                inner,
+            } => {
+                let mut unitary_clauses = Vec::new();
+                let mut inner_pattern = pattern.clone();
+                let mut inner_inj = inj.to_vec();
+                if_pattern.eval_circ(&mut inner_pattern, &mut inner_inj, &mut unitary_clauses);
+                let temp: Vec<_> = unitary_clauses.iter().rev().map(ClauseC::invert).collect();
+                clauses.extend(unitary_clauses);
+
+                inner.eval_circ_clause(&inner_pattern, &inner_inj, phase_mul, clauses);
+
+                clauses.extend(temp)
+            }
+            TermT::Gate { def, .. } => {
+                def.eval_circ_clause(pattern, inj, phase_mul, clauses);
+            }
+            TermT::Inverse(inner) => {
+                inner.eval_circ_clause(pattern, inj, -phase_mul, clauses);
+            }
+            TermT::Sqrt(inner) => {
+                inner.eval_circ_clause(pattern, inj, phase_mul / 2.0, clauses);
+            }
+        }
+    }
 }
+
